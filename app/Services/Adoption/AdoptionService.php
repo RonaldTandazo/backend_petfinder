624edb63a677a2d9b44865429a72a23a704ec2fb
RonaldTandazo <edu09085@hotmail.com>
2026-09-06 06:17:11 +0000

    quitando logs innecesarios

--- app/Services/Adoption/AdoptionService.php
+++ app/Services/Adoption/AdoptionService.php
@@ -7,7 +7,6 @@ use App\Models\Pet;
 use App\Models\PetFollows;
 use App\Services\Storage\PictureDeletionService;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ValidatedInput;
 
 class AdoptionService
